7445bc902c01a4c14eff82381377b746ba4314f7
JahongirJuraev <jahongirjuraev202@gmail.com>
2023-09-21 17:20:45 +0500

    First project

---
+++ main.js
@@ -0,0 +1,16 @@
+let number = document.getElementById('number');
+let button = document.getElementById('generateBtn');
+
+const randomGenerateNumber = () =>{
+    let generateNumber = Math.floor(Math.random() * 14 + 1)
+    number.textContent = generateNumber;
+    
+    if(generateNumber == 13){
+        alert('My favourite Number is 13')
+        number.style.color = "red";
+    }else{
+        number.style.color = "#000000c7";
+    }
+}
+
+button.addEventListener('click', randomGenerateNumber);
